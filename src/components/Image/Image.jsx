e3844b5b58f69c45957f38af94f7d3a246e336da
Jacob Krus <coneter@mail.ru>
2024-03-15 20:06:37 +0300

    git init

---
+++ src/components/Image/Image.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import styles from "./styles.module.css";
+import { formatTimeAgo } from "../helpers/formatTimeAgo";
+
+const Image = ({ image }) => {
+  return (
+    <div className={styles.wrapper}>{image ? <img src={image} alt="news" className={styles.image} /> : null}</div>
+  );
+};
+
+export default Image;
